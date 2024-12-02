93b7d438733b90897dc2fba2ce6b67a23d735393
selvitopu <uselvitopu@yahoo.com>
2024-12-02 05:04:42 +0300

    data.sql was added

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,1 +1,4 @@
-select * from roles
+insert into credit.public.role
+values ((select max(id) + 1 from role), false, 'ADMIN');
+insert into credit.public.role
+values ((select max(id) + 1 from role), true, 'CUSTOMER');
